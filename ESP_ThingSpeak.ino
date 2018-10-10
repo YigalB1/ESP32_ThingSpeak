7c405e88b3e9742fdec99cd59dcb7ddf23407c81
YigalB <yigalb@hotmail.com>
2018-10-10 23:18:14 +0300

    Update ESP_ThingSpeak.ino
    Change LED pin number

--- ESP_ThingSpeak.ino
+++ ESP_ThingSpeak.ino
@@ -8,8 +8,8 @@
 
 #include <WiFi.h>
 
-#define buttonPin 0 
-#define LEDPin 5
+#define buttonPin 0
+#define LEDPin 2
 
 // Network information
 char* ssid = "WIFINAME";
@@ -28,7 +28,7 @@ int measurementNumber = 0;
 
 
 void setup(){
-  
+
     Serial.begin(115200);
     pinMode(buttonPin,INPUT);
     pinMode(LEDPin, OUTPUT);
@@ -41,7 +41,7 @@ const int numberPoints = 7;
 float wifiStrength;
 
   // In each loop, make sure there is always an internet connection.
-    if (WiFi.status() != WL_CONNECTED) { 
+    if (WiFi.status() != WL_CONNECTED) {
         connectWiFi();
     }
 
@@ -49,12 +49,12 @@ float wifiStrength;
     if (digitalRead(buttonPin) == LOW){
         if (millis() - lastConnectionTime > postingInterval) {
             blinkX(2,250); // Verify the button press.
-            wifiStrength = getStrength(numberPoints); 
+            wifiStrength = getStrength(numberPoints);
             httpRequest(wifiStrength, measurementNumber);
             blinkX(measurementNumber,200);  // Verify that the httpRequest is complete.
             measurementNumber++;
         }
-        
+
     }
 }
 // Connect your device to the wireless network using the connectWiFi function. The device blinks five times fast once connected.
@@ -68,30 +68,30 @@ void connectWiFi(){
 
     // Show the user a connection is successful.
     Serial.println("Connected");
-    blinkX(5,50);  
+    blinkX(5,50);
 }
 // Connect to the ThingSpeak server and build the data strings for the HTTP POST command using the httpRequest function.
 
 void httpRequest(float field1Data, int field2Data) {
 
     WiFiClient client;
-    
+
     if (!client.connect(server, 80)){
-      
+
         Serial.println("connection failed");
         lastConnectionTime = millis();
         client.stop();
-        return;     
+        return;
     }
-    
+
     else{
-        
+
         // create data string to send to ThingSpeak
         String data = "field1=" + String(field1Data) + "&field2=" + String(field2Data); //shows how to include additional field data in http post
-        
+
         // POST data to ThingSpeak
         if (client.connect(server, 80)) {
-          
+
             client.println("POST /update HTTP/1.1");
             client.println("Host: api.thingspeak.com");
             client.println("Connection: close");
@@ -102,9 +102,9 @@ void httpRequest(float field1Data, int field2Data) {
             client.print(data.length());
             client.print("\n\n");
             client.print(data);
-            
+
             Serial.println("RSSI = " + String(field1Data));
-            lastConnectionTime = millis();   
+            lastConnectionTime = millis();
         }
     }
     client.stop();
@@ -115,7 +115,7 @@ void httpRequest(float field1Data, int field2Data) {
 int getStrength(int points){
     long rssi = 0;
     long averageRSSI=0;
-    
+
     for (int i=0;i < points;i++){
         rssi += WiFi.RSSI();
         delay(20);
@@ -127,16 +127,16 @@ int getStrength(int points){
 // Finally, use the blinkX function to flash the device LED. The flashes allow the board to communicate with you when not connected to the computer via USB.
 
 // Flash the LED a variable number of times with a variable delay.
-void blinkX(int numTimes, int delayTime){ 
+void blinkX(int numTimes, int delayTime){
     for (int g=0;g < numTimes;g++){
 
         // Turn the LED on and wait.
-        digitalWrite(LEDPin, HIGH);  
+        digitalWrite(LEDPin, HIGH);
         delay(delayTime);
 
         // Turn the LED off and wait.
         digitalWrite(LEDPin, LOW);
         delay(delayTime);
-        
+
     }
 }
